e66430353bb562971e0f9721f68eb165048e3dd9
Kari Hagemeier <kari@hagemeier.cloud>
2026-02-25 20:18:30 +0100

    feat: refresh Neovim configuration and plugin lockfiles

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,47 @@
--- Reuse the existing Vim configuration for Neovim
-vim.cmd('source ' .. vim.fn.expand('~/.vimrc'))
+-- Configure Neovim to use LazyVim as the configuration baseline.
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo(
+      {
+        {
+          "Unable to clone lazy.nvim. Check git/network access and restart Neovim.",
+          "Error",
+        },
+      },
+      true,
+      {}
+    )
+    return
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "plugins" },
+  },
+  defaults = { lazy = true },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+})
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,3 @@
+-- User override plugin definitions for LazyVim.
+-- Keep this module to let LazyVim import custom plugins safely.
+return {}
